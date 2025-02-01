78e3ffc40de1e55eb960319cd0c60c5729041def
Joshi79 <j.haas1990@web.de>
2025-02-01 22:14:29 +0100

    - Updated Neighbour Sampler

--- GraphCSC/neigh_samplers.py
+++ GraphCSC/neigh_samplers.py
@@ -1,8 +1,5 @@
 from layers import Layer
-from utils import load_centrality_measures
-import numpy
 import tensorflow as tf
-import random
 
 
 class NeighborSampler(Layer):
@@ -28,22 +25,20 @@ class NeighborSampler(Layer):
         max_node_id = tf.shape(self.centrality_tensor)[0] - 1
         adj_lists = tf.clip_by_value(adj_lists, 0, max_node_id)
 
-        # Shape: [batch_size, max_degree]
 
         # Gather centrality scores for neighbors
-        centrality_scores = tf.gather(self.centrality_tensor, adj_lists)  # Shape: [batch_size, max_degree]
+        centrality_scores = tf.gather(self.centrality_tensor, adj_lists)
 
         # Sort neighbors based on centrality scores
         _, sorted_indices = tf.nn.top_k(centrality_scores, k=tf.shape(centrality_scores)[1], sorted=True)
 
-        # Reorder adjacency list based on sorted indices
         batch_size = tf.shape(adj_lists)[0]
         max_degree = tf.shape(adj_lists)[1]
         batch_indices = tf.tile(tf.expand_dims(tf.range(batch_size), axis=1), [1, max_degree])
         gather_indices = tf.stack([batch_indices, sorted_indices], axis=-1)
-        sorted_adj_lists = tf.gather_nd(adj_lists, gather_indices)  # Shape: [batch_size, max_degree]
+        sorted_adj_lists = tf.gather_nd(adj_lists, gather_indices)
 
         # Slice to get the top-k neighbors
-        top_neighbors = tf.slice(sorted_adj_lists, [0, 0], [-1, num_samples])  # Shape: [batch_size, num_samples]
+        top_neighbors = tf.slice(sorted_adj_lists, [0, 0], [-1, num_samples])
 
         return top_neighbors
